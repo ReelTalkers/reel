2f7f6282671f9c764891fbc857b7afab9d0bdbfa
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2017-02-21 14:23:00 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # reel-web
 The front end website of Reel
+
+##Setup
+```sh
+npm install
+```
+
+##Run
+```sh
+npm start
+```
+Open `index.html`
---
+++ modules/main.js
@@ -0,0 +1,4 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(<h1>Welcome to Reeltalk!</h1>, document.body);
