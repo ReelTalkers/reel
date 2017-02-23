5c10115cc3dee5e4d8f392f4e26aa79b0bf0bcf9
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2017-02-23 16:53:18 -0500

    Change the state after login.

--- modules/main.js
+++ modules/main.js
@@ -3,28 +3,55 @@ import ReactDOM from 'react-dom';
 import FacebookLogin from 'react-facebook-login';
 import {APP_ID} from '../keys';
 
-const responseFacebook = (response) => {
-  console.log(response);
-}
-
 class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      accessToken: null,
+      email: null,
+      expiresIn: null,
+      id: null,
+      name: null,
+      picture: null,
+      signedRequest: null,
+      userID: null,
+    }
+  };
+
+  responseFacebook(response) {
+    console.log(response);
+    this.setState({
+      accessToken: response.accessToken,
+      email: response.email,
+      expiresIn: response.expiresIn,
+      id: response.id,
+      name: response.name,
+      picture: response.picture,
+      signedRequest: response.signedRequest,
+      userID: response.userID,
+    });
   };
 
   render() {
-    return (
-      <div>
-        <h1>Welcome to Reeltalk!</h1>
-        <FacebookLogin
-          appId={APP_ID}
-          autoLoad={true}
-          fields="name,email,picture"
-          cssClass="fb-login"
-          textButton="Sign in with Facebook"
-          callback={responseFacebook} />
-      </div>
-    );
+    if (this.state.name) {
+      return (
+        <div>
+          <h1>Welcome to Reeltalk, {this.state.name}!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <FacebookLogin
+            appId={APP_ID}
+            autoLoad={true}
+            fields="name,email,picture"
+            cssClass="fb-login"
+            textButton="Sign in with Facebook"
+            callback={this.responseFacebook.bind(this)} />
+        </div>
+      );
+    }
   }
 }
 
