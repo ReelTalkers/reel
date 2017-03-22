1d3329d019a85e3948cea3c0b30c028bb7fb1407
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2017-03-22 17:21:42 -0400

    Use dummy data.

--- components/Lolomo.js
+++ components/Lolomo.js
@@ -1,16 +1,25 @@
 var React = require('react');
 
+import genres from '../testing/dummy_data.js';
+
 class Lolomo extends React.Component {
   constructor() {
     super();
+    this.state = {
+      genres: JSON.parse(genres)
+    }
   }
 
   render() {
     return (
       <div className="lolomo">
-        <Lomo title="Top Picks"/>
-        <Lomo title="Comedy"/>
-        <Lomo title="Drama"/>
+        {this.state.genres.map(genre =>
+          <Lomo
+            key={genre.title}
+            title={genre.title}
+            movies={genre.movies}
+          />
+        )}
       </div>
     );
   }
@@ -22,15 +31,12 @@ class Lomo extends React.Component {
       <div className="lomo">
         <div className="title">{this.props.title}</div>
         <div className="movies">
-          <Movie/>
-          <Movie/>
-          <Movie/>
-          <Movie/>
-          <Movie/>
-          <Movie/>
-          <Movie/>
-          <Movie/>
-          <Movie/>
+          {this.props.movies.map(movie =>
+            <Movie
+              key={movie.poster_path}
+              poster_path={movie.poster_path}
+            />
+          )}
         </div>
       </div>
     );
@@ -39,9 +45,10 @@ class Lomo extends React.Component {
 
 class Movie extends React.Component {
   render() {
+    const poster_url = "https://image.tmdb.org/t/p/w300_and_h450_bestv2/" + this.props.poster_path;
     return (
       <div className="movie">
-
+        <img src={poster_url}/>
       </div>
     );
   }
---
+++ testing/dummy_data.js
@@ -0,0 +1,109 @@
+var genres = `[
+  {
+    "title": "Top Picks",
+    "movies": [
+      {
+        "poster_path": "/qAwFbszz0kRyTuXmMeKQZCX3Q2O.jpg"
+      },
+      {
+        "poster_path": "/mWOotrG1MMKP9iCy2uPepbu27jk.jpg"
+      },
+      {
+        "poster_path": "/z09QAf8WbZncbitewNk6lKYMZsh.jpg"
+      },
+      {
+        "poster_path": "/gri0DDxsERr6B2sOR1fGLxLpSLx.jpg"
+      },
+      {
+        "poster_path": "/hLudzvGfpi6JlwUnsNhXwKKg4j.jpg"
+      },
+      {
+        "poster_path": "/s9ye87pvq2IaDvjv9x4IOXVjvA7.jpg"
+      },
+      {
+        "poster_path": "/4PiiNGXj1KENTmCBHeN6Mskj2Fq.jpg"
+      },
+      {
+        "poster_path": "/tIKFBxBZhSXpIITiiB5Ws8VGXjt.jpg"
+      },
+      {
+        "poster_path": "/qjiskwlV1qQzRCjpV0cL9pEMF9a.jpg"
+      },
+      {
+        "poster_path": "/kSBXou5Ac7vEqKd97wotJumyJvU.jpg"
+      }
+    ]
+  },
+  {
+    "title": "Comedy",
+    "movies": [
+      {
+        "poster_path": "/inVq3FRqcYIRl2la8iZikYYxFNR.jpg"
+      },
+      {
+        "poster_path": "/sM33SANp9z6rXW8Itn7NnG1GOEs.jpg"
+      },
+      {
+        "poster_path": "/ylXCdC106IKiarftHkcacasaAcb.jpg"
+      },
+      {
+        "poster_path": "/WLQN5aiQG8wc9SeKwixW7pAR8K.jpg"
+      },
+      {
+        "poster_path": "/5qVD5TD1CiALR5vUsMzh2BschVU.jpg"
+      },
+      {
+        "poster_path": "/pbJ1vNwK9X6ZQm2vxarxcLJ0aGn.jpg"
+      },
+      {
+        "poster_path": "/w90cORtNWQAd2k93aFlIBbpQYTC.jpg"
+      },
+      {
+        "poster_path": "/hU0E130tsGdsYa4K9lc3Xrn5Wyt.jpg"
+      },
+      {
+        "poster_path": "/gxK80WUwkQePpXpzRdpob0i7DD5.jpg"
+      },
+      {
+        "poster_path": "/fnaGmGCWgGOQjCpM0TqKPZJ5x85.jpg"
+      }
+    ]
+  },
+  {
+    "title": "Drama",
+    "movies": [
+      {
+        "poster_path": "/6cbIDZLfwUTmttXTmNi8Mp3Rnmg.jpg"
+      },
+      {
+        "poster_path": "/i0t7F6b4R1wURRESAiw9VJNuVoV.jpg"
+      },
+      {
+        "poster_path": "/afhAUuWVB7k7PjJUd4lwO3rzhSq.jpg"
+      },
+      {
+        "poster_path": "/h6O5OE3ueRVdCc7V7cwTiQocI7D.jpg"
+      },
+      {
+        "poster_path": "/vOipe2myi26UDwP978hsYOrnUWC.jpg"
+      },
+      {
+        "poster_path": "/utGq0tOcAPjm5DNMMAbDZi0JTik.jpg"
+      },
+      {
+        "poster_path": "/iMkl2Akc1f4CSJCieVwczM4KjZR.jpg"
+      },
+      {
+        "poster_path": "/n8WNMt7stqHUZEE7bEtzK4FwrWe.jpg"
+      },
+      {
+        "poster_path": "/pq4I860JAol0hFXp2H4VFA2lrU6.jpg"
+      },
+      {
+        "poster_path": "/aeiVxTSTeGJ2ICf1iSDXkF3ivZp.jpg"
+      }
+    ]
+  }
+]`
+
+export default genres;
