a744ac5745ff7951d286ac0559ac0b469eeef880
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2017-03-23 13:14:01 -0400

    Add circle with plus

--- components/Group.js
+++ components/Group.js
@@ -4,12 +4,18 @@ class Group extends React.Component {
   constructor() {
     super();
   }
-
+// Make profile photo its own react class
+// make circle its own react class
   render() {
     return (
       <div className="group">
-        <div className="round-image-wrapper">
-          <img src={this.props.user_picture.data.url}/>
+        <div className="photos">
+          <div className="round-image-wrapper">
+            <img src={this.props.user_picture.data.url}/>
+          </div>
+          <div className="add">
+            <div className="plus"/>
+          </div>
         </div>
         <div className="group-name">You</div>
       </div>
