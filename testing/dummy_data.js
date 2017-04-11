610662a7618b3b3cf9e3fb63409842eb7227b76c
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2017-04-10 23:07:45 -0400

    Improve data load

--- testing/dummy_data.js
+++ testing/dummy_data.js
@@ -1,109 +1,140 @@
-var genres = `[
+var data = `{
+"recommendations" : [
   {
-    "title": "Top Picks",
-    "movies": [
+    "genre": "Top Picks",
+    "media": [
       {
+        "id": "tt4882174",
         "poster_path": "/qAwFbszz0kRyTuXmMeKQZCX3Q2O.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/mWOotrG1MMKP9iCy2uPepbu27jk.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/z09QAf8WbZncbitewNk6lKYMZsh.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/gri0DDxsERr6B2sOR1fGLxLpSLx.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/hLudzvGfpi6JlwUnsNhXwKKg4j.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/s9ye87pvq2IaDvjv9x4IOXVjvA7.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/4PiiNGXj1KENTmCBHeN6Mskj2Fq.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/tIKFBxBZhSXpIITiiB5Ws8VGXjt.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/qjiskwlV1qQzRCjpV0cL9pEMF9a.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/kSBXou5Ac7vEqKd97wotJumyJvU.jpg"
       }
     ]
   },
   {
-    "title": "Comedy",
-    "movies": [
+    "genre": "Comedy",
+    "media": [
       {
+        "id": "tt4882174",
         "poster_path": "/inVq3FRqcYIRl2la8iZikYYxFNR.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/sM33SANp9z6rXW8Itn7NnG1GOEs.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/ylXCdC106IKiarftHkcacasaAcb.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/WLQN5aiQG8wc9SeKwixW7pAR8K.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/5qVD5TD1CiALR5vUsMzh2BschVU.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/pbJ1vNwK9X6ZQm2vxarxcLJ0aGn.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/w90cORtNWQAd2k93aFlIBbpQYTC.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/hU0E130tsGdsYa4K9lc3Xrn5Wyt.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/gxK80WUwkQePpXpzRdpob0i7DD5.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/fnaGmGCWgGOQjCpM0TqKPZJ5x85.jpg"
       }
     ]
   },
   {
-    "title": "Drama",
-    "movies": [
+    "genre": "Drama",
+    "media": [
       {
+        "id": "tt4882174",
         "poster_path": "/6cbIDZLfwUTmttXTmNi8Mp3Rnmg.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/i0t7F6b4R1wURRESAiw9VJNuVoV.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/afhAUuWVB7k7PjJUd4lwO3rzhSq.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/h6O5OE3ueRVdCc7V7cwTiQocI7D.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/vOipe2myi26UDwP978hsYOrnUWC.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/utGq0tOcAPjm5DNMMAbDZi0JTik.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/iMkl2Akc1f4CSJCieVwczM4KjZR.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/n8WNMt7stqHUZEE7bEtzK4FwrWe.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/pq4I860JAol0hFXp2H4VFA2lrU6.jpg"
       },
       {
+        "id": "tt4882174",
         "poster_path": "/aeiVxTSTeGJ2ICf1iSDXkF3ivZp.jpg"
       }
     ]
   }
-]`
+]}`
 
-export default genres;
+export default data;
