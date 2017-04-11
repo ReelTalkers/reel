18adfd83651b60302ede8961019f88f83d85c431
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2017-04-10 23:08:01 -0400

    Make movies change state on click

--- components/Lolomo.js
+++ components/Lolomo.js
@@ -1,23 +1,14 @@
 var React = require('react');
 
-import genres from '../testing/dummy_data.js';
-
 class Lolomo extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      genres: JSON.parse(genres)
-    }
-  }
-
   render() {
     return (
       <div className="lolomo">
-        {this.state.genres.map(genre =>
+        {this.props.recommendations.map(genre =>
           <Lomo
-            key={genre.title}
-            title={genre.title}
-            movies={genre.movies}
+            key={genre.genre}
+            title={genre.genre}
+            movies={genre.media}
           />
         )}
       </div>
@@ -33,7 +24,9 @@ class Lomo extends React.Component {
         <div className="movies">
           {this.props.movies.map(movie =>
             <Movie
+              // TODO: change the key to be the movie.id
               key={movie.poster_path}
+              id={movie.id}
               poster_path={movie.poster_path}
             />
           )}
@@ -43,11 +36,26 @@ class Lomo extends React.Component {
   }
 }
 
+// I think have an onclick here that outlines the movie and calls a callback to lomo
+// that puts the detail below the lomo
 class Movie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isActive: false,
+    }
+  }
+
+  toggleActive() {
+    this.setState({
+      isActive: !this.state.isActive
+    })
+  }
+
   render() {
     const poster_url = "https://image.tmdb.org/t/p/w300_and_h450_bestv2/" + this.props.poster_path;
     return (
-      <div className="movie">
+      <div onClick={() => this.toggleActive()} className={"movie"+ (this.state.isActive? " active" : "")}>
         <img src={poster_url}/>
       </div>
     );
