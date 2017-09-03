80a504b3b60cd23dcaa50fc15d4d3c2dda81a2cf
Greg Ziegan <greg.ziegan@gmail.com>
2017-09-03 12:49:29 -0700

    add password to local user

--- config/dev.exs
+++ config/dev.exs
@@ -52,7 +52,7 @@ config :phoenix, :stacktrace_depth, 20
 config :reel, Reel.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "",
+  password: "reel",
   database: "reel_dev",
   hostname: "localhost",
   template: "template0",
