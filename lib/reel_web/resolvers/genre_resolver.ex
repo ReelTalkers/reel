3b2f82d106ebe5aee2f9501ed768e30dfbf0cd08
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2017-09-04 20:20:25 -0700

    Simplify genre resolver partial code to remove joins

--- lib/reel_web/resolvers/genre_resolver.ex
+++ lib/reel_web/resolvers/genre_resolver.ex
@@ -26,12 +26,7 @@ defmodule ReelWeb.GenreResolver do
 
   def partial(args, _info) do
     genres = Genre
-      |> Ecto.Query.distinct(true)
       |> where([g], g.id in ^args[:genres])
-      |> join(:inner, [g], mg in MediumGenre, g.id == mg.genre_id)
-      |> join(:inner, [g, mg], m in Medium, m.id == mg.medium_id)
-      |> join(:left, [g, mg, m], mv in Movie, mv.medium_id == m.id)
-      |> join(:left, [g, mg, m, mv], s in Show, s.medium_id == m.id)
       |> Repo.all
       |> Repo.preload([media: [:movie, :show]])
       |> Enum.map(&to_graphql/1)
